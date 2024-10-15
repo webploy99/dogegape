39b2f2fb634a859b38f805de1a09ffbd6d0a6037
webdevk28 <webdevk28@gmail.com>
2024-10-15 14:32:17 +0700

    update

--- script.js
+++ script.js
@@ -12,3 +12,33 @@
       this.className += " active";
       });
     }
+
+
+    async function checkKoreanUser() {
+      try {
+        const response = await fetch('https://ipapi.co/json/');
+        const data = await response.json();
+        const isKoreanIP = data.country === 'KR';
+  
+        const koreanRegions = ['Seoul', 'Busan', 'Incheon', 'Daegu', 'Gwangju', 'Daejeon', 'Ulsan'];
+          const isKoreanRegion = koreanRegions.includes(data.region);
+          const isKoreanLanguage = navigator.language.startsWith('ko');
+  
+        if (isKoreanIP && isKoreanLanguage && isKoreanRegion) {
+          document.getElementById('Korea').style.display = 'block';
+          document.getElementById('NoKorea').style.display = 'none';
+  
+        } 
+        else if (data.ip === '116.212.159.240'){
+          document.getElementById('Korea').style.display = 'block';
+          document.getElementById('NoKorea').style.display = 'none';
+        }
+        else {
+          document.getElementById('Korea').style.display = 'none';
+          document.getElementById('NoKorea').style.display = 'block';
+        }
+      } catch (error) {
+        console.error('Error fetching IP or location data:', error);
+      }
+    }
+    checkKoreanUser();
